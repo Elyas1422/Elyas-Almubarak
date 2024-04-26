ed9e8526a80622120cce779f4745dff7b77486e1
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-04-26 11:23:42 +0300

    add intro section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { IntroComponent } from './components/intro/intro.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [CommonModule, RouterOutlet, NavbarComponent, IntroComponent],
 })
 export class AppComponent {
   title = 'Elyas-Almubarak';
