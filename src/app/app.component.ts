d765b626ed6202da8567d0f282a5a08e117ec882
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-04-26 22:24:46 +0300

    Adding skills section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { IntroComponent } from './components/intro/intro.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, NavbarComponent, IntroComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    IntroComponent,
+    SkillsComponent,
+  ],
 })
 export class AppComponent {
-  title = 'Elyas-Almubarak';
+  title = 'Elyas Almubarak';
 }
---
+++ src/app/components/skills/skills.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skills',
+  standalone: true,
+  imports: [],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.scss',
+})
+export class SkillsComponent {
+  frontendSkills = [
+    { name: 'HTML', icon: 'devicon-html5-plain' },
+    { name: 'CSS', icon: 'devicon-css3-plain' },
+    { name: 'JavaScript', icon: 'devicon-javascript-plain' },
+    { name: 'TypeScript', icon: 'devicon-typescript-plain' },
+    { name: 'Angular', icon: 'devicon-angular-plain' },
+    { name: 'Tailwind CSS', icon: 'devicon-tailwindcss-original' },
+    { name: 'Figma', icon: 'devicon-figma-plain' },
+    { name: 'PrimeNg', icon: 'prime-icon' },
+  ];
+  backendSkills = [
+    { name: 'Node.js', icon: 'devicon-nodejs-plain-wordmark' },
+    { name: 'Express.js', icon: 'devicon-express-original' },
+    { name: 'MongoDB', icon: 'devicon-mongodb-plain' },
+    { name: 'MySQL', icon: 'devicon-mysql-plain' },
+    { name: 'Swagger', icon: 'devicon-swagger-plain' },
+    { name: 'Git', icon: 'devicon-git-plain' },
+    { name: 'FastAPI', icon: 'devicon-fastapi-plain' },
+  ];
+}
