d7b1416c076269fe878584641c8176a772c4f432
Elyas Almubarak <79568124+Elyas1422@users.noreply.github.com>
2024-04-27 18:09:50 +0300

    Add gsap animation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Import BrowserAnimationsModule
 
 import { routes } from './app.routes';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
+    provideHttpClient(withFetch()),
+  ], // Add BrowserAnimationsModule to providers
 };
--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,5 +1,16 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import { DOCUMENT } from '@angular/common';
 
+gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-skills',
   standalone: true,
@@ -7,7 +18,7 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss',
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit {
   frontendSkills = [
     { name: 'HTML', icon: 'devicon-html5-plain' },
     { name: 'CSS', icon: 'devicon-css3-plain' },
@@ -21,11 +32,28 @@ export class SkillsComponent {
   backendSkills = [
     { name: 'Node.js', icon: 'devicon-nodejs-plain-wordmark' },
     { name: 'Express.js', icon: 'devicon-express-original' },
-    { name: 'MongoDB', icon: 'devicon-mongodb-plain' },
     { name: 'MySQL', icon: 'devicon-mysql-plain' },
     { name: 'Swagger', icon: 'devicon-swagger-plain' },
     { name: 'Git', icon: 'devicon-git-plain' },
     { name: 'Python', icon: 'devicon-python-plain' },
     { name: 'FastAPI', icon: 'devicon-fastapi-plain' },
   ];
+
+  constructor(
+    public el: ElementRef,
+    @Inject(DOCUMENT) private document: Document
+  ) {}
+
+  ngOnInit(): void {
+    gsap.set('#cardsContainer', { opacity: 0, y: 100 });
+
+    gsap.to('#cardsContainer', {
+      scrollTrigger: '#cardsContainer',
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      delay: 0.25,
+      ease: 'power2.inOut',
+    });
+  }
 }
